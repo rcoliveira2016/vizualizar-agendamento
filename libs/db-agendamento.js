43174cd29a9ec177c5627e3d1d487ce6f6c2a57c
rcoliveira2016 <roramon7@gmail.com>
2020-12-20 12:38:58 -0300

    Ajustes

---
+++ libs/db-agendamento.js
@@ -0,0 +1,34 @@
+let sqlite3 = require('sqlite3');
+const sqlite = require('sqlite');
+require('dotenv/config');
+
+const nomeArquivoSqlite = process.env.NOME_ARQUIVO_SQLITE;
+const pastaArquivoSqlite = process.env.PASTA_ARQUIVO_SQLITE;
+
+const dbDefult = {
+    naoIniciado:true
+};
+
+module.exports = {
+    db: dbDefult,
+    abrir: async function(){
+        this.db = await sqlite.open({ filename: `${pastaArquivoSqlite}/${nomeArquivoSqlite}`, driver: sqlite3.Database });
+    },
+    fechar: async function(){
+        if(this.db.naoIniciado) return;
+
+        await this.db.close()
+    },
+    all: async function (sql, params) {
+        try {
+            await this.abrir();
+            return await this.db.all(sql);
+        } catch (error) {
+            return {erro:error.toString()}
+        }
+        finally{
+            await this.fechar();            
+        }
+        
+    }
+};
---
+++ libs/multer-base-dados-sqlite.js
@@ -0,0 +1,38 @@
+let multer = require('multer');
+let path = require('path');
+const fs = require('fs')
+require('dotenv/config');
+
+const nomeArquivoSqlite = process.env.NOME_ARQUIVO_SQLITE;
+const pastaArquivoSqlite = process.env.PASTA_ARQUIVO_SQLITE;
+const caminho = `${pastaArquivoSqlite}/${nomeArquivoSqlite}`;
+
+let storageBaseDadosSqlite = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, pastaArquivoSqlite)
+    },
+    filename: function (req, file, cb) {
+        try {
+            if (fs.existsSync(caminho)) {
+                fs.unlinkSync(caminho)
+            }
+        } catch(err) {
+            console.error(err)
+        }        
+        cb(null, nomeArquivoSqlite)
+    }
+  })
+   
+let uploadBaseDadosSqlite = multer({ 
+    storage: storageBaseDadosSqlite,
+    fileFilter: function (req, file, callback) {
+        var ext = path.extname(file.originalname);
+        if(ext !== '.db') {
+            return callback(null, false)
+        }
+        callback(null, true)
+    },
+})
+
+module.exports = uploadBaseDadosSqlite;
+  
